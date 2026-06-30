797b39f0d28699220139ceb4f9e2406085b58af2
Ha Phuong <phuong.vuha@8seneca.com>
2026-06-30 09:36:04 +0700

    fix: fix home marquee image not render on iOS/Safari

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const directusUrl = process.env.DIRECTUS_URL;
 const directusHost = directusUrl ? new URL(directusUrl).hostname : undefined;
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ['192.168.88.164'],
   images: {
     remotePatterns: [
       ...(directusHost
